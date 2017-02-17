677d010dceb39098b871478ba4c688b21c05bf3d
Oliver Ponder <oliver.ponder@gmail.com>
2017-02-17 10:06:23 +0100

    Add a server and live reloading on edits to md files

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
 	gulp
 
+serve:
+	gulp serve
+
 clean:
 	rm -rf exports/*
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,11 @@ var fs = require('fs');
 var path = require('path');
 var rename = require("gulp-rename");
 var phantom = require('phantom');
-
+var connect = require('gulp-connect');
 var template = fs.readFileSync("layout/index.html").toString();
 
-var templatePlugin = through.obj(
+var templatePlugin = function() {
+  return through.obj(
   function (file, encoding, done) {
     var content = file.contents.toString();
     var mergedContent = template.replace('<!-- MD FILE INSERT -->', content);
@@ -19,20 +20,24 @@ var templatePlugin = through.obj(
 
     this.push(file);
     return done();
-});
+  })
+};
+
+gulp.task('assets', function() {
+  return gulp.src('layout/*/**')
+    .pipe(gulp.dest('exports/layout'));
+})
 
 gulp.task('html', function(){
-  gulp.src('layout/*/**')
-  .pipe(gulp.dest('exports/layout'));
-
   return gulp.src('content/*/**.md')
-    .pipe(templatePlugin) // This is what places the markdown into the html layout
+    .pipe(templatePlugin()) // This is what places the markdown into the html layout
     .pipe(rename(function (path) {
       path.dirname += "/" + path.basename;
       path.basename = "index";
       path.extname = ".html"
     }))
     .pipe(gulp.dest('exports'))
+    .pipe(connect.reload());
 });
 
 var phantom = require("phantom");
@@ -63,4 +68,22 @@ phantom.create().then(ph => {
 //     .pipe(gulp.dest('build/html'))
 // });
 
-gulp.task('default', [ 'html']);
+gulp.task('connect', function() {
+  return connect.server({
+    root: 'exports',
+    livereload: true,
+    port: 8000
+  });
+});
+
+gulp.task('watch', function () {
+  return gulp.watch(['./content/*/**'], ['build']);
+});
+
+gulp.task('reload', function() {
+  return gulp.src("").pipe(connect.reload());
+});
+
+gulp.task('default', ['build']);
+gulp.task('build', ['assets', 'html']);
+gulp.task('serve', ['build', 'connect', 'watch']);
